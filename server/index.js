65797189c03c541bdabc952cf72d3628dfe106ca
1mpuser <alex022003@mail.ru>
2022-07-03 22:41:13 +0300

    Devices could be pushed into database

--- server/index.js
+++ server/index.js
@@ -6,10 +6,14 @@ const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const fileUpload = require('express-fileupload');
+const path = require('path');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //json format is for better understanding whats going on
+app.use(express.static(path.resolve(__dirname, 'static'))); // route + path for take a closer look at the file
+app.use(fileUpload({})); // to upload file to interact them and put to dadtabase
 app.use('/api', router);
 
 //middleware should be in the end last error handler
